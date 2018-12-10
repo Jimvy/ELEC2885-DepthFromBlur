37bc4e34cd466d921f6d1f1ac2e325fe2f16976e
Jean-Martin Vlaeminck <jmartin.vlaeminck@gmail.com>
2018-12-10 14:45:22 +0100

    Probably correct single-depth cost-computing function

--- deconvolve/deconvolve.py
+++ deconvolve/deconvolve.py
@@ -1,48 +1,108 @@
+import sys
+
 import pywt
 
 import numpy as np
+from scipy import signal
+from scipy import misc
 import matplotlib.pyplot as plt
-from pyunlocbox import solvers
+from pyunlocbox import solvers, functions
 
 from PIL import Image
 
-img = Image.open("DFB_artificial_dataset/im0_blurry.bmp")
-
-# Restore Image using Richardson-Lucy algorithm
-coeffs = pywt.wavedec2(img, 'db1')
-img2 = pywt.waverec2(coeffs, 'db1')
-
-solver = solvers.forward_backward()
-n = len(img)
-x0 = np.zeros(n)
+filter_type = None
 
 
-def f1(img):
-    return 0
+def gaussian_filter(shape=(3, 3), sigma=0.5):
+    m, n = [(ss-1.)/2. for ss in shape]
+    y, x = np.ogrid[-m:m+1, -n:n+1]
+    h = np.exp( -(x*x + y*y)/(2. *sigma*sigma))
+    h[h < np.finfo(h.dtype).eps*h.max()] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    return h
 
 
-def f2(img):
-    return 0
+def circular_filter(radius=5):
+    cradius = np.ceil(radius)
+    x, y = np.ogrid[-cradius:cradius+1, -cradius:cradius+1]
+    disk = x**2 + y**2 <= radius**2
+    return disk.astype(float)
 
 
-def f3(img):
-    return 0
+def run(pathname, filter_type='gaussian', wavelet_type='haar'):
+
+    wavelet = pywt.Wavelet(wavelet_type)
+
+    def compute_approx_single_depth(alpha, coeff_slice, d):
+        coeffs = pywt.array_to_coeffs(alpha, coeff_slice, 'wavedec2')
+        X = pywt.waverec2(coeffs, wavelet=wavelet)
+        k = None
+        if filter_type == 'gaussian':
+            k = gaussian_filter(sigma=d)
+        else:
+            k = circular_filter(radius=d)
+        Y = signal.convolve2d(X, k)
+        return Y
+
+    img = misc.imread(pathname)
+    print(img)
+    print(type(img))
+    img = Image.open(pathname)
+    print(img)
+    print(type(img))
+    img = np.array(img)
+    print(img)
+    print(type(img))
+    img_no_blurry = Image.open(pathname.replace('blurry', 'original'))
+
+    coeffs = pywt.wavedec2(img_no_blurry, wavelet)
+    alpha, coeffs_slice = pywt.coeffs_to_array(coeffs)
+    Y = compute_approx_single_depth(alpha, coeffs_slice, 3)
+    # img2 = pywt.waverec2(coeffs, wavelet)
+
+    solver = solvers.forward_backward()
+    n = len(img)
+    x0 = np.zeros(n)
 
 
-ret = solvers.solve([f1, f2, f3], x0, solver, rtol=1e-4, maxit=300)
+    def error_term():
+        return 0
 
-fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(80, 50))
-plt.gray()
 
-for a in (ax[0], ax[1], ax[2]):
-    a.axis('off')
+    def alpha_sparsity(img):
+        return 0
 
-ax[0].imshow(img)
-ax[0].set_title('Blurry Data')
 
-ax[1].imshow(img2)
-ax[1].set_title('reconstruct')
+    def mask_sparsity(img):
+        return 0
 
-fig.subplots_adjust(wspace=0.02, hspace=0.2,
-                    top=0.9, bottom=0.05, left=0, right=1)
-plt.show()
+    ret = solvers.solve([], x0, solver, rtol=1e-4, maxit=300)
+
+    fig, ax = plt.subplots(nrows=1, ncols=3, figsize=(80, 50))
+    plt.gray()
+
+    for a in (ax[0], ax[1], ax[2]):
+        a.axis('off')
+
+    ax[0].imshow(img)
+    ax[0].set_title('Blurry Data')
+
+    ax[1].imshow(Y)
+    ax[1].set_title('reconstruct')
+
+    #ax[2].imshow(img_noblurry)
+    #ax[2].set_title('original')
+
+    fig.subplots_adjust(wspace=0.02, hspace=0.2,
+                        top=0.9, bottom=0.05, left=0, right=1)
+    plt.show()
+
+if __name__ == '__main__':
+    if len(sys.argv) > 3:
+        run(sys.argv[1], sys.argv[2], sys.argv[3])
+    elif len(sys.argv) > 2:
+        run(sys.argv[1], sys.argv[2])
+    else:
+        run(sys.argv[1])
